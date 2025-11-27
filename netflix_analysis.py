09a67ad766fd1dc28d7ef3d12318a6addb30c02d
berfinerk <berfinnerk@gmail.com>
2025-11-27 13:36:41 +0300

    Film ve diziler için türlere göre ortalama süreyi hesapla ve görselleştir

--- netflix_analysis.py
+++ netflix_analysis.py
@@ -219,6 +219,75 @@ plt.legend()
 plt.grid(True)
 # plt.show()
 
+#süreye göre tür analizi
+print(df_netflix["duration"].head(20))
+
+def parse_duration(duration):
+    if pd.isna(duration) or duration == 0:
+        return 0
+    duration = str(duration)
+    if 'min' in duration:
+        return int(duration.replace('min', ''))
+    elif 'Season' in duration:
+        return int(duration.split(' ')[0])
+    else:
+        return 0
+#yeni numeric sütun
+df_netflix['duration_num'] = df_netflix['duration'].apply(parse_duration)
+
+#kontrol: ilk 20 değer
+print(df_netflix[["duration","duration_num"]].head(20))
+
+#film ve dizi ayırma
+df_movies = df_netflix[df_netflix["type"] == "Movie"]
+df_tvshows = df_netflix[df_netflix["type"] == "TV Show"]
+
+#kontrol
+# print("Film sayısı:" , len(df_movies))
+# print("Dizi Sayısı", len(df_tvshows))
+
+#film türlerine göre ortalama süre
+
+avg_duration_movies = {}
+for cat in categories:
+# o türdeki filmlerin duration_num ortalaması
+    avg_duration_movies[cat] = df_movies[df_movies[cat]==1]["duration_num"].mean()
+
+avg_duration_movies = pd.Series(avg_duration_movies).sort_values(ascending=False)
+avg_duration_movies_int = avg_duration_movies.round(0).astype(int)
+print("--- Film Türleri Ortalama Süre (Dakika) ---")
+print(avg_duration_movies_int)
+
+avg_duration_tv = {}
+for cat in categories:
+    avg_duration_tv[cat] = df_tvshows[df_tvshows[cat]==1]["duration_num"].mean()
+
+avg_duration_tv =pd.Series(avg_duration_tv).sort_values(ascending=False)
+avg_duration_tv_int = avg_duration_tv.fillna(0).round(0).astype(int)
+print("--- TV Show Türleri Ortalama Süre (Sezon Sayısı) ---")
+print(avg_duration_tv_int)
+
+#film
+plt.figure(figsize = (12,6))
+avg_duration_movies_int.plot(kind="bar",color="skyblue")
+plt.title("Film Türlerine Göre Ortalama Süre (Dakika)")
+plt.xlabel("Tür")
+plt.ylabel("Ortalama Dakika")
+plt.xticks(rotation=45, ha='right')
+plt.tight_layout()
+plt.show()
+
+#dizi
+plt.figure(figsize = (12,6))
+avg_duration_tv.plot(kind="bar",color="skyblue")
+plt.title("Dizi Türlerine Göre Ortalama Süre (Sezon)")
+plt.xlabel("Tür")
+plt.ylabel("Ortalama Sezon")
+plt.xticks(rotation=45, ha='right')
+plt.tight_layout()
+plt.show()
+
+
 
 
 
